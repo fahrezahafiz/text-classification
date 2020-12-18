50e0540f5a136635de0dbc866dc6abf02e99a5a8
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-12-19 00:51:52 +0700

    text data retrieved

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.jinja')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        print('get method')
+        return render_template('index.jinja')
+    elif request.method == 'POST':
+        if bool(request.form):
+            data = request.form['text-input']
+        elif bool(request.files):
+            data = request.files['file-input']
+        print(request.form)
+        print(request.files)
+        return render_template('index.jinja')
